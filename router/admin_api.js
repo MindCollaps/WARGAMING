07e83058e1858650793755dbb6d684d00ea1df92
CrazyTim71 <118295691+CrazyTim71@users.noreply.github.com>
2025-02-13 20:56:50 +0100

    code cleanup

--- router/admin_api.js
+++ router/admin_api.js
@@ -2,12 +2,10 @@ import express from 'express';
 import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
-import { url } from 'inspector';
 import {exec} from 'child_process'
 
 const router = express.Router();
 
-// Setup temporary storage
 const tmpStorage = multer.diskStorage({
     destination: function (req, file, cb) {
         const tmpDir = './tmp';
@@ -15,7 +13,6 @@ const tmpStorage = multer.diskStorage({
         cb(null, tmpDir);
     },
     filename: function (req, file, cb) {
-        // Use the original filename
         cb(null, file.originalname);
     }
 });
@@ -41,25 +38,20 @@ router.get('/', async (req, res) => {
 
 // /api/admin
 router.post('/upload_background_image', upload.single('image'), async (req, res) => {
-    // Check if a file was uploaded
     if (!req.file) {
         return res.status(400).json({
             status: '400',
-            response: "No file uploaded"
+            message: "No file uploaded"
         });
     }
 
-    // create temp dir
     const backgroundDir = './public/assets/background';
     fs.mkdirSync(backgroundDir, { recursive: true });
     
-    // change the extension to .png
     const parsedPath = path.parse(req.file.originalname);
     const newFilename = parsedPath.name + '.png';
 
-    // set the output dir to /assets/background
     const outputPath = path.join(backgroundDir, newFilename);
-    
     const command = `convert ${req.file.path} "${outputPath}"`;
     
     exec(command, (err, stdout, stderr) => {
@@ -73,12 +65,8 @@ router.post('/upload_background_image', upload.single('image'), async (req, res)
                 }
             )
         }
-    
-        console.log("stdout:", stdout);
 
-        // Check if converted file exists
         fs.access(outputPath, fs.constants.F_OK, (err) => {
-            // Clean up tmp file
             fs.unlink(req.file.path, (unlinkErr) => {
                 if (unlinkErr) console.error('Error deleting tmp file:', unlinkErr);
             });
@@ -90,7 +78,6 @@ router.post('/upload_background_image', upload.single('image'), async (req, res)
                     error: stdout.trim().split('\n'),
                 });
             } else {
-                // Respond with success and the file pathof the uploaded image
                 res.json({
                     status: '200',
                     message: "File uploaded and converted",
